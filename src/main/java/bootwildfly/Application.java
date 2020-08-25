0ca52f9f3803be5bf3100a50a52f31948ae2e1a1
Andrea-Raise <63711659+Andrea-Raise@users.noreply.github.com>
2020-08-25 17:33:01 +0200

    Update Application.java

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -15,10 +15,5 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-    
 }
 
